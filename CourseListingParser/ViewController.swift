8b504329a38fa8db4c4842b928c6d77827914712
Jordan <sk.jordan@foxmail.com>
2020-07-25 09:20:57 -0500

    Initial Commit

---
+++ CourseListingParser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseListingParser
+//
+//  Created by Zhuoran Sun on 2020/7/25.
+//  Copyright © 2020 washu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
